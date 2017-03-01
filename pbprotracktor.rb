c97272eeeb34e6b77a3f5bc69c78c0a524a3407f
Dave Rice <dave@dericed.com>
2017-03-01 16:44:23 -0500

    pbprotracktor requires jq

--- pbprotracktor.rb
+++ pbprotracktor.rb
@@ -5,7 +5,9 @@ class Pbprotracktor < Formula
   url "https://github.com/amiaopensource/pbprotracktor/archive/v20170227.tar.gz"
   sha256 "081e27ba510eeb6d157ca282508c8ffc06707fdd354157c36df2161c9606fe5b" 
   head 'https://github.com/amiaopensource/pbprotracktor.git'
+  revision 1
 
+  depends_on "jq"
   depends_on "mysql"
   depends_on "postgresql"
 
